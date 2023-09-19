16c7b2a8e06cc35dde040797eb9c8ee11c3112e5
xin0nix <xin0nix@yahoo.com>
2023-09-19 21:52:18 +0300

    Refactoring

--- leetcode.com/problems/min-cost-climbing-stairs/solution.cpp
+++ leetcode.com/problems/min-cost-climbing-stairs/solution.cpp
@@ -11,17 +11,15 @@ int BottomUpSolution::minCostClimbingStairs(std::vector<int> &cost) {
   // Note: the algorithm works in both directions, because we can start either
   // from the 0th, or 1th element, which is the same as making 1 or 2 jump
 
-  int l = 0, r = cost.front();
+  int d1 = 0, d2 = 0;
   // we can either make single jump, or double from cur to l, r
-  for (size_t i = 1; i != cost.size(); ++i) {
-    int c = cost[i];
-    c += std::min(l, r);
-    // l, r, c
-    l = r;
-    // _, l, c+min(l, r), -> next iteration
-    r = c;
+  for (int c : cost) {
+    // [c, .., d2], [c, d1]
+    c += std::min(d2, d1);
+    d2 = d1;
+    d1 = c;
   }
-  return std::min(l, r);
+  return std::min(d2, d1);
 }
 
 int BFSolution::minCostClimbingStairs(std::vector<int> &cost) {
