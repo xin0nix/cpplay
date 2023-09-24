8e2170fcba770475148cd1976b57bad256c36fde
xin0nix <xin0nix@yahoo.com>
2023-09-24 15:54:21 +0300

    Update docs

--- leetcode.com/problems/valid-sudoku/solution.hpp
+++ leetcode.com/problems/valid-sudoku/solution.hpp
@@ -12,6 +12,8 @@ struct SolutionImpl {
 
 // Runtime: 7 ms, beats 99%
 // Memory: 18 MB, beats 70%
+// NOTE: came up with the solution completely on my own, which is good
+// tag: #my-solution
 struct BasicSolution : SolutionImpl {
   virtual bool isValidSudoku(vector<vector<char>> &board) override final;
 };
