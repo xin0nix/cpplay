8ff5fa4f1f08095feb4c389541377f59dcde6e75
xin0nix <xin0nix@yahoo.com>
2024-03-12 09:36:36 +0300

    Custom solution for reorder list

--- leetcode/reorder-list/solution_test.cpp
+++ leetcode/reorder-list/solution_test.cpp
@@ -1,5 +1,9 @@
+#include <algorithm>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <vector>
 using namespace std;
+using namespace testing;
 // Definition for singly-linked list.
 struct ListNode {
   int val;
@@ -26,12 +30,74 @@ struct ListWrapper final {
   [[nodiscard]] ListNode *getHead() const { return list.front().next; }
 };
 
+vector<int> buildVec(ListNode *head) {
+  vector<int> result;
+  if (!head)
+    return {};
+  while (head) {
+    result.push_back(head->val);
+    head = head->next;
+  }
+  return result;
+}
+
 class Solution {
 public:
   void reorderList(ListNode *head) {
-    ListNode *from = head;
-    ListNode *to = nullptr;
+    ListNode *l = head;
+    ListNode *r = nullptr;
     ListNode base;
-    // TODO: implement solution using 2 pointers
+    ListNode *h = &base;
+    while (l) {
+      h->next = l;
+      h = h->next;
+      if (l->next == r) [[unlikely]]
+        break;
+      l = l->next;
+      ListNode *k = l;
+      while (k != r && k->next != r) [[likely]]
+        k = k->next;
+      h->next = k;
+      h = h->next;
+      r = k;
+    }
+    h->next = nullptr;
   }
 };
+
+TEST(ReorderListTest, LeetCodeExample1) {
+  ListWrapper lw({1, 2, 3, 4});
+  Solution().reorderList(lw.getHead());
+  auto res = buildVec(lw.getHead());
+  EXPECT_THAT(res, ElementsAre(1, 4, 2, 3));
+}
+
+TEST(ReorderListTest, LeetCodeExample2) {
+  ListWrapper lw({1, 2, 3, 4, 5});
+  Solution().reorderList(lw.getHead());
+  auto res = buildVec(lw.getHead());
+  EXPECT_THAT(res, ElementsAre(1, 5, 2, 4, 3));
+}
+
+TEST(ReorderListTest, Empty) {
+  ListWrapper lw({});
+  Solution().reorderList(lw.getHead());
+  auto res = buildVec(lw.getHead());
+  EXPECT_THAT(res, ElementsAre());
+}
+
+TEST(ReorderListTest, Single) {
+  ListWrapper lw({1});
+  Solution().reorderList(lw.getHead());
+  auto res = buildVec(lw.getHead());
+  EXPECT_THAT(res, ElementsAre(1));
+}
+
+// int main() {
+//   ListWrapper lw({1, 2, 3});
+//   Solution().reorderList(lw.getHead());
+//   auto res = buildVec(lw.getHead());
+//   for (auto r : res)
+//     cout << r << ", ";
+//   cout << endl;
+// }
