96f4f32d35537182192f67a3e20a4b44e79abc43
xin0nix <xin0nix@yahoo.com>
2024-04-15 18:42:22 +0300

    Solve LRU cache

--- leetcode/lru-cache/solution_test.cpp
+++ leetcode/lru-cache/solution_test.cpp
@@ -1,3 +1,5 @@
+#include "gmock/gmock.h"
+#include <deque>
 #include <functional>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -10,34 +12,91 @@
 using namespace std;
 using ::testing::ElementsAre;
 
-struct LRUCache final {
+class LRUCache final {
   struct Node {
+    Node() = default;
+    explicit Node(int key_, int val_) : key(key_), val(val_) {}
+    int key;
+    int val;
     Node *prev{nullptr};
     Node *next{nullptr};
-    int val{0};
   };
 
-  int mCapacity;
-  // L <--------------------> R
-  // leastUsed ... ... mostUsed
-  Node *leastUsed{nullptr};
-  Node *mostUsed{nullptr};
-  unordered_map<int, Node *> kvMap;
+  const int mCapacity;
+  // L <-----------------------> R
+  // least ... node, node ... most
+  Node mLeastRecentlyUsed;
+  Node mMostRecentlyUsed;
+  unordered_map<int, Node *> mKeyNodeMap;
+  deque<Node> mNodes;
 
-  LRUCache(int capacity) : mCapacity(capacity) {}
+public:
+  LRUCache(int capacity) : mCapacity(capacity) {
+    // Restore the pointers
+    mLeastRecentlyUsed.next = &mMostRecentlyUsed;
+    mMostRecentlyUsed.prev = &mLeastRecentlyUsed;
+    // Clear the memory
+    mKeyNodeMap.clear();
+    mNodes.clear();
+  }
 
   int get(int key) {
-    auto it = kvMap.find(key);
-    if (it == kvMap.end())
+    auto it = mKeyNodeMap.find(key);
+    if (it == mKeyNodeMap.end())
       return -1;
     Node *n = it->second;
-    // TODO: move to the right-most position
+    // Move to the most recent position
+    evict(n);
+    inject(n);
     return n->val;
   }
 
   void put(int key, int value) {
-    // TODO: evict the least recently used item if necessary
-    // TODO: put the new node to the right-most position
+    auto it = mKeyNodeMap.find(key);
+    // The key is already present, update the node and move up
+    if (it != mKeyNodeMap.end()) {
+      Node *refreshed = it->second;
+      refreshed->val = value;
+      evict(refreshed);
+      inject(refreshed);
+      assert(mNodes.size() == mKeyNodeMap.size());
+      return;
+    }
+    // Allocate new node and make it most recently used
+    if (mNodes.size() < mCapacity) {
+      Node &newNode = mNodes.emplace_back(key, value);
+      mKeyNodeMap[key] = &newNode;
+      inject(&newNode);
+      assert(mNodes.size() == mKeyNodeMap.size());
+      return;
+    }
+    // Repurpose the least recenlty used node
+    Node *replacement = mLeastRecentlyUsed.next;
+    mKeyNodeMap.erase(replacement->key);
+    replacement->key = key;
+    replacement->val = value;
+    mKeyNodeMap[key] = replacement;
+    evict(replacement);
+    inject(replacement);
+    assert(mNodes.size() == mKeyNodeMap.size());
+  }
+
+private:
+  /// Remove node from the list without deallocation
+  void evict(Node *node) {
+    Node *prev = node->prev;
+    Node *next = node->next;
+    prev->next = next;
+    next->prev = prev;
+  }
+
+  /// Put node to the most recently used place, without allocation
+  void inject(Node *node) {
+    Node *prev = mMostRecentlyUsed.prev;
+    prev->next = node;
+    node->prev = prev;
+    mMostRecentlyUsed.prev = node;
+    node->next = &mMostRecentlyUsed;
   }
 };
 
@@ -72,7 +131,30 @@ struct LRUCacheHandle {
 
 TEST(LRUCacheTest, LeetCodeExample1) {
   LRUCacheHandle lch;
+  {
+
+    SCOPED_TRACE("LeetCodeExample1");
+    lch.check({"LRUCache", "put", "put", "get", "put", "get", "put", "get",
+               "get", "get"},
+              {{2}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {4, 4}, {1}, {3}, {4}});
+    EXPECT_THAT(lch.results, ElementsAre(nullopt, nullopt, nullopt, 1, nullopt,
+                                         -1, nullopt, -1, 3, 4));
+  }
+}
+
+TEST(LRUCacheTest, LeetCodeBug1) {
+  LRUCacheHandle lch;
   lch.check({"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get",
              "get"},
-            {{2}, {1, 1}, {2, 2}, {1}, {3, 3}, {2}, {4, 4}, {1}, {3}, {4}});
+            {{2}, {1, 0}, {2, 2}, {1}, {3, 3}, {2}, {4, 4}, {1}, {3}, {4}});
+  EXPECT_THAT(lch.results, ElementsAre(nullopt, nullopt, nullopt, 0, nullopt,
+                                       -1, nullopt, -1, 3, 4));
+}
+
+TEST(LRUCacheTest, LeetCodeBug2) {
+  LRUCacheHandle lch;
+  lch.check({"LRUCache", "put", "put", "get", "put", "put", "get"},
+            {{2}, {2, 1}, {2, 2}, {2}, {1, 1}, {4, 1}, {2}});
+  EXPECT_THAT(lch.results,
+              ElementsAre(nullopt, nullopt, nullopt, 2, nullopt, nullopt, -1));
 }
