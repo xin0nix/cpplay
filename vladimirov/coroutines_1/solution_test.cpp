97f70ade5d559b257cb6ff70f7159227aecf68cc
xin0nix <xin0nix@yahoo.com>
2024-07-20 19:20:07 +0300

    Finish micro example coroutines

--- vladimirov/coroutines_1/solution_test.cpp
+++ vladimirov/coroutines_1/solution_test.cpp
@@ -1,32 +1,44 @@
 #include <coroutine>
+
 #include <gtest/gtest.h>
-#include <iostream>
 
-namespace {
-struct promise;
+namespace example_1 {
+std::string global;
+struct resumable {
+  struct promise_type {
+    using coro_handle = std::coroutine_handle<promise_type>;
+    auto get_return_object() { return coro_handle::from_promise(*this); }
+    std::suspend_always initial_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void return_void() {}
+    void unhandled_exception() { std::terminate(); }
+  };
+  using coro_handle = std::coroutine_handle<promise_type>;
+  resumable(coro_handle handle) : handle_(handle) {}
+  bool resume() {
+    if (!handle_.done())
+      handle_.resume();
+    return !handle_.done();
+  }
+  resumable(resumable &&) = default;
+  resumable(const resumable &) = delete;
+  ~resumable() { handle_.destroy(); }
 
-struct resumable : std::coroutine_handle<promise> {
-  using promise_type = ::promise;
+private:
+  coro_handle handle_;
 };
-
-struct promise {
-  resumable get_return_object() { return {resumable::from_promise(*this)}; }
-  std::suspend_always initial_suspend() noexcept { return {}; }
-  std::suspend_always final_suspend() noexcept { return {}; }
-  void return_void() {}
-  void unhandled_exception() {}
-};
-} // namespace
 
 resumable foo() {
-  std::cout << "Hello" << std::endl;
+  global += "Hello";
   co_await std::suspend_always();
-  std::cout << "World" << std::endl;
+  global += "World";
 }
+} // namespace example_1
 
 TEST(CoroutineTestSuite, Resumable) {
-  auto t = foo();
+  auto t = example_1::foo();
   t.resume();
-  std::cout << "Coroutine" << std::endl;
+  example_1::global += " CoRoutine ";
   t.resume();
+  ASSERT_EQ(example_1::global, "Hello CoRoutine World");
 }
