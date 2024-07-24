95600deca168c97da8aaaf4f404824d8334b4a01
xin0nix <xin0nix@yahoo.com>
2024-07-24 18:49:32 +0300

    Skip over chapter 8

--- satter-40/chapter-8/solution_test.cpp
+++ satter-40/chapter-8/solution_test.cpp
@@ -1,1 +1,33 @@
+#include <gtest/gtest.h>
 
+#include <memory>
+
+namespace detail {
+enum class Called {
+  Nothing,
+  Destructible,
+  Ordinary,
+} called;
+template <std::destructible T> void checked_delete(T *ptr) noexcept {
+  if (ptr) {
+    std::destroy_at(ptr);
+    delete ptr;
+    return;
+  }
+  called = Called::Nothing;
+}
+} // namespace detail
+
+TEST(Chapter8Tests, DestructibleTest1) {
+  using namespace detail;
+  called = Called::Nothing;
+  struct Cat {
+    ~Cat() { called = Called::Destructible; }
+  };
+  auto *cat = new Cat{};
+  checked_delete(cat);
+  ASSERT_EQ(called, Called::Destructible);
+  cat = nullptr;
+  ASSERT_NO_FATAL_FAILURE(checked_delete(cat));
+  ASSERT_EQ(called, Called::Nothing);
+}
