3a3bdda342f0f9c9bdbc883982a36fecf3183690
xin0nix <xin0nix@yahoo.com>
2024-07-30 21:17:25 +0300

    Implement basic socket funcs

--- simulators/train_booking/src/TcpSocket.cpp
+++ simulators/train_booking/src/TcpSocket.cpp
@@ -1,3 +1,39 @@
 #include "TcpSocket.hpp"
 
+#include <arpa/inet.h>
+#include <errno.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
+
+namespace tcp_socket {
+
+TcpSocket::TcpSocket() {
+  if (mFileDescriptor =
+          ::socket(/*domain=ip4*/ AF_INET, /*type=tcp*/ SOCK_STREAM,
+                   /*default*/ 0);
+      mFileDescriptor == -1)
+    throw std::string(::strerror(errno));
+}
+
+TcpSocket::~TcpSocket() noexcept {
+  if (auto error = ::close(mFileDescriptor); error)
+    std::cerr << ::strerror(errno)
+              << std::endl; // FIXME: replace with boost logv2
+}
+
+void TcpSocket::bind(std::string ipAddress, const uint16_t port) {
+  struct ::sockaddr_in addr4in;
+  std::memset(&addr4in, 0, sizeof(addr4in));
+  addr4in.sin_family = AF_INET;
+  addr4in.sin_port = ::htons(port);
+  addr4in.sin_addr.s_addr = inet_addr(ipAddress.c_str());
+  struct ::sockaddr *addr4 = reinterpret_cast<struct ::sockaddr *>(&addr4in);
+  if (auto error = ::bind(mFileDescriptor, addr4, sizeof(struct sockaddr_in));
+      error)
+    throw std::string(strerror(errno));
+}
+
+} // namespace tcp_socket
--- simulators/train_booking/src/TcpSocket.hpp
+++ simulators/train_booking/src/TcpSocket.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <string>
+
 namespace tcp_socket {
-struct TcpSocket {
+struct TcpSocket final {
   /// Create a new TCP socket over IP network
   TcpSocket();
+  /// Close the fd associated with the TCP socket
+  ~TcpSocket() noexcept;
+  /// Assign a name to a socket
+  void bind(std::string ipAddress, const uint16_t port);
 
 private:
   /// File descriptor associated with the socket
