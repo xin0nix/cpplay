846a47df7d7f72a6756dae165c0921658b61a643
xin0nix <xin0nix@yahoo.com>
2024-08-10 19:30:47 +0300

    Add new get seats test

--- simulators/train_booking/naive/test/ExchangeFormatTest.cpp
+++ simulators/train_booking/naive/test/ExchangeFormatTest.cpp
@@ -4,8 +4,13 @@
 #include <gtest/gtest.h>
 #include <range/v3/all.hpp>
 
+using ::testing::ElementsAre;
+
 TEST(ExchangeFormatTest, RequestVacantSeats) {
   exchange_format::Request request;
   request.mutable_vacant_seats()->mutable_carriage()->set_carriage_id(3);
   auto var = app::fromRequest(request);
+  ASSERT_TRUE(std::holds_alternative<app::request::VacantSeats>(var));
+  auto seats = std::get<app::request::VacantSeats>(var);
+  ASSERT_THAT(seats.cars, ElementsAre(app::Car{3}));
 }
