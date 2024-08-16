a9ae310cc1c53df311581b909d8afc00ede92199
xin0nix <xin0nix@yahoo.com>
2024-08-16 21:55:47 +0300

    Implement bottom-up solution

--- leetcode/permutations/solution_test.cpp
+++ leetcode/permutations/solution_test.cpp
@@ -1,3 +1,4 @@
+#include "gtest/gtest.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -8,7 +9,7 @@
 using ::testing::UnorderedElementsAre;
 using namespace std;
 
-namespace {
+namespace detail {
 std::vector<std::string> stringify(const std::vector<std::vector<int>> &input) {
   std::vector<std::string> result;
   result.reserve(input.size());
@@ -26,9 +27,9 @@ std::vector<std::string> stringify(const std::vector<std::vector<int>> &input) {
   }
   return result;
 }
-} // namespace
+} // namespace detail
 
-struct Solution {
+struct TopDownSolution {
   struct Impl {
     Impl(const std::vector<int> &nums) : mNums(nums) {}
 
@@ -66,15 +67,72 @@ struct Solution {
   }
 };
 
-TEST(Permutations, MetaTest) {
-  auto res = ::stringify({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+struct BottomUpSolution {
+  struct Impl {
+    vector<int> &mNums;
+    std::vector<vector<int>> visit(size_t level) {
+      int curLevelItem = mNums.at(level);
+      if (level == mNums.size() - 1) {
+        return {{curLevelItem}};
+      }
+      auto nextLevelPerms = visit(level + 1);
+      vector<vector<int>> curLevemPerms;
+      for (const auto &subPerm : nextLevelPerms) {
+        for (size_t i = 0U; i <= subPerm.size(); ++i) {
+          auto perm = subPerm;
+          perm.insert(perm.cbegin() + i, curLevelItem);
+          curLevemPerms.push_back(std::move(perm));
+        }
+      }
+      return curLevemPerms;
+    }
+  };
+  vector<vector<int>> permute(vector<int> &nums) {
+    return Impl{nums}.visit(0U);
+  }
+};
+
+template <typename T> struct PermutationsTest : ::testing::Test {
+  T mSolution;
+  T &get() { return mSolution; }
+};
+
+struct NameGenerator {
+  template <typename T> static std::string GetName(int) {
+    if constexpr (std::is_same_v<T, TopDownSolution>)
+      return "top-down";
+    if constexpr (std::is_same_v<T, BottomUpSolution>)
+      return "bottom-up";
+  }
+};
+
+using Types = ::testing::Types<TopDownSolution, BottomUpSolution>;
+
+TYPED_TEST_SUITE(PermutationsTest, Types, NameGenerator);
+
+TYPED_TEST(PermutationsTest, MetaTest) {
+  auto res = ::detail::stringify({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
   EXPECT_THAT(res, UnorderedElementsAre("[1,2,3]", "[4,5,6]", "[7,8,9]"));
 }
 
-TEST(Permutations, Leet1) {
+TYPED_TEST(PermutationsTest, Leet1) {
   vector nums{1, 2, 3};
-  auto perms = Solution().permute(nums);
-  auto res = stringify(perms);
+  auto perms = this->get().permute(nums);
+  auto res = ::detail::stringify(perms);
   EXPECT_THAT(res, UnorderedElementsAre("[1,2,3]", "[1,3,2]", "[2,1,3]",
                                         "[2,3,1]", "[3,1,2]", "[3,2,1]"));
+}
+
+TYPED_TEST(PermutationsTest, Leet2) {
+  vector nums{1, 2};
+  auto perms = this->get().permute(nums);
+  auto res = ::detail::stringify(perms);
+  EXPECT_THAT(res, UnorderedElementsAre("[1,2]", "[2,1]"));
+}
+
+TYPED_TEST(PermutationsTest, Leet3) {
+  vector nums{1};
+  auto perms = this->get().permute(nums);
+  auto res = ::detail::stringify(perms);
+  EXPECT_THAT(res, UnorderedElementsAre("[1]"));
 }
