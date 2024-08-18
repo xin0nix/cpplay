a64bb478ed69284a3cd974e92131edcea380d761
xin0nix <xin0nix@yahoo.com>
2024-08-18 10:07:37 +0300

    Resolve longest common prefix

--- leetcode/longest-common-prefix/solution_test.cpp
+++ leetcode/longest-common-prefix/solution_test.cpp
@@ -7,6 +7,36 @@
 using std::string;
 using std::vector;
 
+struct BasicSolution {
+  string longestCommonPrefix(const vector<string> &strs) {
+    // Early exit conditions
+    if (strs.empty())
+      return "";
+    if (strs.size() == 1)
+      return strs.front();
+    // Figure out shortest string
+    size_t shortestLen = strs[0].size();
+    for (auto &s : strs)
+      shortestLen = std::min(s.size(), shortestLen);
+    // Iterate over "char columns" and stop on the first mismatch
+    std::stringstream ss;
+    for (size_t charPos = 0U; charPos < shortestLen; ++charPos) {
+      const char cur = strs[0][charPos];
+      bool mismatch = false;
+      for (const auto &s : strs) {
+        if (s[charPos] != cur) {
+          mismatch = true;
+          break;
+        }
+      }
+      if (mismatch)
+        break;
+      ss << cur;
+    }
+    return ss.str();
+  }
+};
+
 struct RangeBasedSolution {
   string longestCommonPrefix(const vector<string> &strs) {
     namespace ranges = std::ranges;
@@ -30,10 +60,22 @@ struct RangeBasedSolution {
   }
 };
 
-TEST(LongesPrefix, Leet) {
-  EXPECT_EQ(
-      RangeBasedSolution().longestCommonPrefix({"flower", "flow", "flight"}),
-      "fl");
-  EXPECT_EQ(RangeBasedSolution().longestCommonPrefix({"dog", "racecar", "car"}),
-            "");
+template <typename S>
+concept SolutionConcept = requires(S s, vector<string> v) {
+  { s.longestCommonPrefix(v) } -> std::convertible_to<string>;
+};
+
+template <SolutionConcept S> struct SolutionTest : ::testing::Test {
+  S sol;
+  S &getSol() { return sol; }
+};
+
+using SolutionTypes = ::testing::Types<RangeBasedSolution, BasicSolution>;
+
+TYPED_TEST_SUITE(SolutionTest, SolutionTypes);
+
+TYPED_TEST(SolutionTest, Leet) {
+  auto &Sol = this->getSol();
+  EXPECT_EQ(Sol.longestCommonPrefix({"flower", "flow", "flight"}), "fl");
+  EXPECT_EQ(Sol.longestCommonPrefix({"dog", "racecar", "car"}), "");
 }
