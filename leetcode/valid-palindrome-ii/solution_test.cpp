e6d301c460792f3231604e105c63de6d048e5310
xin0nix <xin0nix@yahoo.com>
2024-08-30 22:05:00 +0300

    Solve valid palindrome II

--- leetcode/valid-palindrome-ii/solution_test.cpp
+++ leetcode/valid-palindrome-ii/solution_test.cpp
@@ -6,26 +6,27 @@
 using std::string;
 
 struct Solution {
+  [[nodiscard]] bool isPalindrome(const string &str, size_t l,
+                                  size_t r) const noexcept {
+    while (l <= r) {
+      if (str[l] != str[r]) {
+        return false;
+      }
+      l += 1;
+      r -= 1;
+    }
+    return true;
+  }
+
   bool validPalindrome(const string &str) {
     if (str.size() <= 1) {
       return true;
     }
     size_t l = 0;
     size_t r = str.size() - 1;
-    bool missed = false;
     while (l <= r) {
       if (str[l] != str[r]) {
-        if (missed) {
-          return false;
-        }
-        missed = true;
-        if (str[l + 1] == str[r]) {
-          l += 1;
-        } else if (str[l] == str[r - 1]) {
-          r -= 1;
-        } else {
-          return false;
-        }
+        return isPalindrome(str, l + 1, r) || isPalindrome(str, l, r - 1);
       }
       l += 1;
       r -= 1;
@@ -43,7 +44,9 @@ TEST(ValidPalindrome2, Cases) {
     EXPECT_TRUE(Solution().validPalindrome("abca"));
     EXPECT_TRUE(Solution().validPalindrome("aecca"));
     EXPECT_TRUE(Solution().validPalindrome("ahehba"));
-    EXPECT_TRUE(Solution().validPalindrome("bnvfqmgmlcupuufuupuculmgmqfvnb"));
+    EXPECT_TRUE(Solution().validPalindrome("cupuufuupuc"));
+    EXPECT_TRUE(Solution().validPalindrome("ucupuufuupuc"));
+    EXPECT_TRUE(Solution().validPalindrome("cupuufuupucu"));
   }
   {
     SCOPED_TRACE("Negative");
