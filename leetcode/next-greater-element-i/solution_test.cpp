3087545dbd4e59b6419c0481ed739728d8353ae5
xin0nix <xin0nix@yahoo.com>
2024-10-11 06:47:11 +0300

    Optimize Greater Next Element O(NM)

--- leetcode/next-greater-element-i/solution_test.cpp
+++ leetcode/next-greater-element-i/solution_test.cpp
@@ -1,9 +1,42 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <iterator>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 using ::testing::ElementsAre;
+
+namespace {
+template <typename Iterable> struct enumerate_object {
+  Iterable &iterable;
+  using iterator = decltype(std::begin(std::declval<Iterable>()));
+
+  struct Iterator {
+    iterator iter;
+    std::size_t index;
+
+    Iterator(iterator it, std::size_t idx) : iter(it), index(idx) {}
+    auto operator*() const { return std::make_tuple(index, *iter); }
+    Iterator &operator++() {
+      ++iter;
+      ++index;
+      return *this;
+    }
+    bool operator!=(const Iterator &other) const { return iter != other.iter; }
+  };
+
+  enumerate_object(Iterable &it) : iterable(it) {}
+  auto begin() { return Iterator(std::begin(iterable), 0); }
+  auto end() { return Iterator(std::end(iterable), 0); }
+};
+
+template <typename Iterable> auto enumerate(Iterable &&iterable) {
+  return enumerate_object<Iterable>(iterable);
+}
+} // namespace
 
 struct Solution {
   // Function to be tested
@@ -11,9 +44,16 @@ struct Solution {
                                       std::vector<int> &nums2) {
     std::vector<int> result;
     result.reserve(nums1.size());
+    std::unordered_map<int, size_t> val2index;
+    // Store indicies of nums2 in the cache
+    for (auto [index, value] : enumerate(nums2)) {
+      val2index[value] = index;
+    }
+
     // Given nums1[i], find nums2[j] such that nums2[j] == nums1[i]
     for (const auto n : nums1) {
-      auto it = std::find(nums2.cbegin(), nums2.cend(), n);
+      auto it = nums2.cbegin();
+      std::advance(it, val2index[n]);
       // Look up for nums2[k], such that k > j, nums2[k] > nums2[j]
       auto next = std::find_if(it, nums2.cend(), [n](auto x) { return x > n; });
       if (next != nums2.cend()) {
