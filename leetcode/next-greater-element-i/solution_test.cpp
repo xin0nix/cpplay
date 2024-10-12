46a2db76bd5eea24099e1695a5d865b53847229c
xin0nix <xin0nix@yahoo.com>
2024-10-12 21:21:43 +0300

    Finish next greater element Stack solution

--- leetcode/next-greater-element-i/solution_test.cpp
+++ leetcode/next-greater-element-i/solution_test.cpp
@@ -2,7 +2,9 @@
 #include <gtest/gtest.h>
 
 #include <iterator>
+#include <stack>
 #include <tuple>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -38,7 +40,7 @@ template <typename Iterable> auto enumerate(Iterable &&iterable) {
 }
 } // namespace
 
-struct Solution {
+struct BasicSolution {
   // Function to be tested
   std::vector<int> nextGreaterElement(std::vector<int> &nums1,
                                       std::vector<int> &nums2) {
@@ -67,31 +69,92 @@ struct Solution {
   }
 };
 
-TEST(NextGreaterElementTest, BasicTest) {
+struct StackSloution {
+  // Function to be tested
+  std::vector<int> nextGreaterElement(std::vector<int> &nums1,
+                                      std::vector<int> &nums2) {
+    std::vector<int> result(nums1.size(), -1);
+    std::unordered_map<int, size_t> nums1ReverseMap;
+    // Store indicies of nums1 in the cache
+    for (auto [index, value] : enumerate(nums1)) {
+      nums1ReverseMap[value] = index;
+    }
+
+    // Given nums1[i], find nums2[j] such that nums2[j] == nums1[i]
+    std::stack<int> stack;
+    for (const auto [nums2Idx, val] : enumerate(nums2)) {
+      while (stack.size() and val > stack.top()) {
+        const auto stackVal = stack.top();
+        stack.pop();
+        auto nums1It = nums1ReverseMap.find(stackVal);
+        if (nums1It != nums1ReverseMap.end()) {
+          result[nums1It->second] = val;
+        }
+      }
+      stack.push(val);
+    }
+    return result;
+  }
+};
+
+#include <concepts>
+#include <vector>
+
+template <typename S>
+concept NGESolution =
+    requires(S s, std::vector<int> &nums1, std::vector<int> &nums2) {
+      {
+        s.nextGreaterElement(nums1, nums2)
+      } -> std::convertible_to<std::vector<int>>;
+    };
+
+template <NGESolution S> struct NextGreaterElementTest : ::testing::Test {
+  void SetUp() override { solution = S(); }
+
+  S &getSolution() { return solution; }
+
+private:
+  S solution;
+};
+
+struct NameGenerator {
+  template <NGESolution S> static std::string GetName(int) {
+    if constexpr (std::is_same_v<S, BasicSolution>)
+      return "basic";
+    if constexpr (std::is_same_v<S, StackSloution>)
+      return "stack";
+  }
+};
+
+// using SolTypes = ::testing::Types<StackSloution, BasicSolution>;
+using SolTypes = ::testing::Types<StackSloution>;
+TYPED_TEST_SUITE(NextGreaterElementTest, SolTypes, NameGenerator);
+
+TYPED_TEST(NextGreaterElementTest, BasicTest) {
   std::vector<int> nums1 = {4, 1, 2};
   std::vector<int> nums2 = {1, 3, 4, 2};
-  EXPECT_THAT(Solution().nextGreaterElement(nums1, nums2),
-              ElementsAre(-1, 3, -1));
+  auto res = this->getSolution().nextGreaterElement(nums1, nums2);
+  EXPECT_THAT(res, ElementsAre(-1, 3, -1));
 }
 
-TEST(NextGreaterElementTest, EmptyNums1Test) {
+TYPED_TEST(NextGreaterElementTest, EmptyNums1Test) {
   std::vector<int> nums1 = {};
   std::vector<int> nums2 = {1, 2, 3};
   std::vector<int> expected = {};
-  std::vector<int> actual = Solution().nextGreaterElement(nums1, nums2);
-  EXPECT_EQ(expected, actual);
+  auto res = this->getSolution().nextGreaterElement(nums1, nums2);
+  EXPECT_EQ(expected, res);
 }
 
-TEST(NextGreaterElementTest, ExampleTest1) {
+TYPED_TEST(NextGreaterElementTest, ExampleTest1) {
   std::vector<int> nums1 = {1, 3, 2};
   std::vector<int> nums2 = {4, 2, 3, 0, 1, 6, 5};
-  EXPECT_THAT(Solution().nextGreaterElement(nums1, nums2),
-              ElementsAre(6, 6, 3));
+  auto res = this->getSolution().nextGreaterElement(nums1, nums2);
+  EXPECT_THAT(res, ElementsAre(6, 6, 3));
 }
 
-TEST(NextGreaterElementTest, AnotherExampleTest) {
+TYPED_TEST(NextGreaterElementTest, AnotherExampleTest) {
   std::vector<int> nums1 = {1, 3, 2, 6, 5, 0};
   std::vector<int> nums2 = {3, 0, 4, 1, 2, 5, 6};
-  EXPECT_THAT(Solution().nextGreaterElement(nums1, nums2),
+  EXPECT_THAT(this->getSolution().nextGreaterElement(nums1, nums2),
               ElementsAre(2, 4, 5, -1, 6, 4));
 }
