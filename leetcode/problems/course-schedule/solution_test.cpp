ecec6c5984b5885c1f3d9a47dc28aac3102cf93d
xin0nix <xin0nix@yahoo.com>
2025-05-22 17:41:02 +0300

    207. Course Schedule

--- leetcode/problems/course-schedule/solution_test.cpp
+++ leetcode/problems/course-schedule/solution_test.cpp
@@ -1,13 +1,105 @@
 #include <gtest/gtest.h>
 #include <tuple>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-// Forward declaration of the Solution class
-class Solution {
-public:
-  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {}
+#include <ranges>
+struct Solution {
+  struct Impl {
+    /*
+      Функция buildAdjacencyList(P):
+          Инициализировать пустой словарь A
+          Для каждого i ∈ [0, numCurses)
+              A[i] = []
+          Для каждой пары [a, b] в P:
+              Добавить b в A[a]
+    */
+    void buildAdjacencyMap(const int numCourses,
+                           const vector<vector<int>> &prerequisites) {
+      for (auto idx : std::views::iota(0, numCourses)) {
+        dependenceMap[idx] = {};
+      }
+      for (const auto &targetPre : prerequisites) {
+        int target = targetPre[0];
+        int pre = targetPre[1];
+        // > To take course a, you must first take course b.
+        dependenceMap[target].emplace(pre);
+      }
+    }
+
+    /*
+      Функция DFS(N):
+        Если N ∈ V:
+            вернуть "нет"  // найден цикл
+        Если N ∈ S:
+            вернуть "да"   // узел уже обработан, циклов нет
+        Если список смежности N пуст:
+            вернуть "да"
+        Добавить N в V
+        Для каждого соседа bij из A[N]:
+            Если DFS(bij) == "нет":
+                вернуть "нет"
+        Удалить N из V
+        Добавить N в S
+        вернуть "да"
+    */
+    bool dfs(int node, int level = 0) {
+      // Indent output according to recursion level
+      if (visiting.contains(node)) {
+        return false;
+      }
+
+      auto &&adjList = dependenceMap[node];
+      if (seen.contains(node) || adjList.empty()) {
+        return true;
+      }
+
+      for (int adj : adjList) {
+        visiting.emplace(node);
+        if (!dfs(adj, level + 1)) {
+          return false;
+        }
+        visiting.erase(node);
+      }
+
+      seen.emplace(node);
+      return true;
+    }
+
+    /*
+    Инициализировать множества S = ∅, V = ∅
+
+    Для каждого узла N в графе:
+        Если N ∉ S:
+            Если DFS(N) == "нет":
+                вернуть "нет"  // цикл найден, задача неразрешима
+
+    Вернуть "да"  // циклов нет, все курсы можно пройти
+     */
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+      buildAdjacencyMap(numCourses, prerequisites);
+      for (auto idx : std::views::iota(0, numCourses)) {
+        if (seen.contains(idx)) {
+          continue;
+        }
+        visiting.clear();
+        if (not dfs(idx)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    std::unordered_map<int, std::set<int>> dependenceMap;
+    std::unordered_set<int> seen;
+    std::unordered_set<int> visiting;
+  };
+
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+    return Impl{}.canFinish(numCourses, prerequisites);
+  }
 };
 
 // Type alias for test parameters: (numCourses, prerequisites, expected)
